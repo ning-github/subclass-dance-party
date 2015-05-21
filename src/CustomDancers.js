fe73698c59d3641dfb110838c0e33b0084ca7530
ning-github <nxiamail@gmail.com>
2015-05-20 21:43:39 -0700

    added shark dancer, no image

--- src/CustomDancers.js
+++ src/CustomDancers.js
@@ -86,4 +86,33 @@ DogDancer.prototype.step = function(){
   this.$node.animate({top: "+=20"}, 100);
 };
 
+var SharkDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  //this.step();
+  this.$node = $('<img src="dog.png" class="SharkDancer">');
+  this.setPosition(this.top, this.left);
+
+};
+
+SharkDancer.prototype = Object.create(Dancer.prototype);
+SharkDancer.prototype.constructor = SharkDancer;
+SharkDancer.prototype.oldStep = function() {
+  Dancer.prototype.step.call(this);
+};
+
+
+SharkDancer.prototype.step = function(){
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.animate({left: "-=100"}, 1000);
+  this.$node.animate({left: "+=100"}, 1000);
+};
+
 
